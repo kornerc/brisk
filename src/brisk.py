69b5ab2bcdd6b8b89ee1f87f43f6dbb9fef642d8
Clemens Korner <clemens.corny@gmail.com>
2014-08-19 14:47:56 +0200

    Python interface to descriptorExtractor->compute (Brisk)

--- src/brisk.py
+++ src/brisk.py
@@ -5,13 +5,25 @@ import cv2
 import numpy as np
 
 def get_keypoints(img):
-    kp_temp = pybrisk.detect(img)
+    kp_array = pybrisk.detect(img)
     kp = []
-    for i in range(kp_temp.shape[0]):
+    for i in range(kp_array.shape[0]):
         kp.append(cv2.KeyPoint())
-        kp[i].pt = (kp_temp[i, 0], kp_temp[i, 1])
-        kp[i].size = kp_temp[i, 2]
-        kp[i].angle = kp_temp[i, 3]
-        kp[i].response = kp_temp[i, 4]
-    return kp
+        kp[i].pt = (kp_array[i, 0], kp_array[i, 1])
+        kp[i].size = kp_array[i, 2]
+        kp[i].angle = kp_array[i, 3]
+        kp[i].response = kp_array[i, 4]
+    return kp, kp_array
 
+def get_features(img, kp_array):
+    features_and_kp = pybrisk.compute(img, kp_array)
+    features = features_and_kp[0]
+    kp_array_new = features_and_kp[1]
+    kp = []
+    for i in range(kp_array_new.shape[0]):
+        kp.append(cv2.KeyPoint())
+        kp[i].pt = (kp_array_new[i, 0], kp_array_new[i, 1])
+        kp[i].size = kp_array_new[i, 2]
+        kp[i].angle = kp_array_new[i, 3]
+        kp[i].response = kp_array_new[i, 4]
+    return kp, features
--- src/pybrisk.cpp
+++ src/pybrisk.cpp
@@ -40,6 +40,55 @@ PyObject* detect(PyObject *img)
     return ret;
 }
 
+PyObject* compute(PyObject *img, PyObject *keypoints)
+{
+    NDArrayConverter cvt;
+
+    cv::Mat img_rgb = cvt.toMat(img);
+    cv::Mat img_gray;
+    cv::cvtColor(img_rgb, img_gray, CV_BGR2GRAY);
+
+    cv::Mat keypoints_array = cvt.toMat(keypoints);
+    PyObject* ret = PyList_New(2);
+    std::vector<cv::KeyPoint> keypoints_cv;
+
+    size_t num_keypoints = keypoints_array.rows;
+
+    for(size_t i = 0; i < num_keypoints; ++i)
+    {
+        cv::KeyPoint temp;
+        temp.pt.x = keypoints_array.at<float>(i, 0);
+        temp.pt.y = keypoints_array.at<float>(i, 1);
+        temp.size = keypoints_array.at<float>(i, 2);
+        temp.angle = keypoints_array.at<float>(i, 3);
+        temp.response = keypoints_array.at<float>(i, 4);
+        keypoints_cv.push_back(temp);
+    }
+
+    cv::Mat descriptors;
+    cv::Ptr<cv::DescriptorExtractor> descriptorExtractor;
+    descriptorExtractor = new brisk::BriskDescriptorExtractor();
+
+    descriptorExtractor->compute(img_gray, keypoints_cv, descriptors);
+
+    num_keypoints = keypoints_cv.size();
+    cv::Mat ret_keypoints(num_keypoints, 5, cv::DataType<float>::type);
+
+    for(size_t i = 0; i < num_keypoints; ++i)
+    {
+        ret_keypoints.at<float>(i, 0) = keypoints_cv[i].pt.x;
+        ret_keypoints.at<float>(i, 1) = keypoints_cv[i].pt.y;
+        ret_keypoints.at<float>(i, 2) = keypoints_cv[i].size;
+        ret_keypoints.at<float>(i, 3) = keypoints_cv[i].angle;
+        ret_keypoints.at<float>(i, 4) = keypoints_cv[i].response;
+    }
+
+    PyList_SetItem(ret, 0, cvt.toNDArray(descriptors));
+    PyList_SetItem(ret, 1, cvt.toNDArray(ret_keypoints));
+
+    return ret;
+}
+
 static void init()
 {
     Py_Initialize();
@@ -50,4 +99,5 @@ BOOST_PYTHON_MODULE(pybrisk)
 {
     init();
     py::def("detect", detect);
+    py::def("compute", compute);
 }
